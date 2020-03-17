18e75f9677248a2e365ce9248c51ce88796e6138
季德强 <2218968173@qq.com>
2020-03-17 22:14:00 +0800

    app second commit

---
+++ app/src/main/java/com/bigdata/agricultureplatform/myfield/fragment/EstimateFragment.java
@@ -0,0 +1,12 @@
+package com.bigdata.agricultureplatform.myfield.fragment;
+
+import android.view.View;
+
+import com.bigdata.agricultureplatform.base.BaseFragment;
+
+public class EstimateFragment extends BaseFragment {
+    @Override
+    public View initView() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/bigdata/agricultureplatform/myfield/fragment/FieldmanageFragment.java
@@ -0,0 +1,13 @@
+package com.bigdata.agricultureplatform.myfield.fragment;
+
+
+import android.view.View;
+
+import com.bigdata.agricultureplatform.base.BaseFragment;
+
+public class FieldmanageFragment extends BaseFragment {
+    @Override
+    public View initView() {
+        return null;
+    }
+}
